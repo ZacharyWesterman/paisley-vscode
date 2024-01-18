61638e1a3900689e7f836d771c8a3052cc73a7f4
Zachary Westerman <westerman.zachary@gmail.com>
2024-01-18 00:49:53 -0600

    Add diagnostics for debug comments

--- server/src/server.ts
+++ server/src/server.ts
@@ -10,6 +10,7 @@ import {
   TextDocumentPositionParams,
   Diagnostic,
   DiagnosticSeverity,
+  MarkupContent,
 } from "vscode-languageserver/node";
 
 import { TextDocument } from "vscode-languageserver-textdocument";
@@ -31,29 +32,34 @@ connection.onInitialize((params: InitializeParams) => {
   return result;
 });
 
+let diagnostics: Diagnostic[] = []
+
 documents.onDidChangeContent((change) => {
+  diagnostics = []
   validateDebugCommands(change.document)
+  .then(() => registerDebugCommands(change.document))
+  .then(() => connection.sendDiagnostics({ uri: change.document.uri, diagnostics}))
 });
 
 const maxProblems = 500
 let commandTypes = new Map<String, String>() //Keep track of command return types.
+let commandBodies = new Map<String, Array<Function>>() //Keep track of command behavior (for debug purposes)
 
 async function validateDebugCommands(document: TextDocument) : Promise<void>
 {
   commandTypes = new Map<String, String>()
 
   let text = document.getText()
-  let pattern = /#!COMMANDS\b.*/g
+  let pattern = /(#![ \\t]*COMMANDS)\b.*/g
   let m: RegExpExecArray | null
 
   const builtInCommands = ['print', 'sleep', 'time', 'systime', 'sysdate', 'error']
   const returnTypes = ['null', 'boolean', 'number', 'string', 'array', 'any']
 
   let problems = 0
-  let diagnostics: Diagnostic[] = []
   while ((m = pattern.exec(text)) && problems < maxProblems)
   {
-    let line = m[0].substring(10)
+    let line = m[0].substring(m[1].length)
     let c_pattn = /\b[^:,#; \t]+\b|[:;#]/g
     let cmd: RegExpExecArray | null
 
@@ -76,6 +82,7 @@ async function validateDebugCommands(document: TextDocument) : Promise<void>
         if (returnTypes.includes(cmd[0]))
         {
           if (last_cmd !== null) commandTypes.set(last_cmd, cmd[0])
+          last_cmd = null
           continue
         }
         problems++
@@ -83,13 +90,18 @@ async function validateDebugCommands(document: TextDocument) : Promise<void>
         diagnostics.push({
           severity: DiagnosticSeverity.Warning,
           range: {
-            start: document.positionAt(m.index + 10 + cmd.index),
-            end: document.positionAt(m.index + 10 + cmd.index + cmd[0].length)
+            start: document.positionAt(m.index + m[1].length + cmd.index),
+            end: document.positionAt(m.index + m[1].length + cmd.index + cmd[0].length)
           },
           message: `Invalid return type. Must be one of ${returnTypes.join(', ')}.`,
-          source: 'Paisley'
+          source: 'Paisley',
         })
+        last_cmd = null
         continue
+      }
+      else
+      {
+        if (last_cmd !== null) commandTypes.set(last_cmd, 'any')
       }
 
       last_cmd = cmd[0]
@@ -100,18 +112,104 @@ async function validateDebugCommands(document: TextDocument) : Promise<void>
       diagnostics.push({
         severity: DiagnosticSeverity.Warning,
         range: {
-          start: document.positionAt(m.index + 10 + cmd.index),
-          end: document.positionAt(m.index + 10 + cmd.index + cmd[0].length)
+          start: document.positionAt(m.index + m[1].length + cmd.index),
+          end: document.positionAt(m.index + m[1].length + cmd.index + cmd[0].length)
         },
         message: `"${cmd[0]}" is a built-in command whose behavior should not be overridden.`,
-        source: 'Paisley'
+        source: 'Paisley',
       })
     }
-  }
 
-  connection.sendDiagnostics({ uri: document.uri, diagnostics})
+    if (last_cmd !== null && !commandTypes.get(last_cmd)) commandTypes.set(last_cmd, 'any')
+  }
 }
 
+async function registerDebugCommands(document: TextDocument) : Promise<void>
+{
+  commandBodies = new Map<String, Array<Function>>()
+
+  let text = document.getText()
+  let pattern = /(#![ \t]*DEBUG[ \t]+)([^:,#; \t]+)\b(.*?\bEND\b)?/g
+  let m: RegExpExecArray | null
+
+  const builtInCommands = ['print', 'sleep', 'time', 'systime', 'sysdate', 'error']
+
+  let problems = 0
+  while ((m = pattern.exec(text)) && problems < maxProblems)
+  {
+    if (builtInCommands.includes(m[2]))
+    {
+      problems++
+
+      diagnostics.push({
+        severity: DiagnosticSeverity.Warning,
+        range: {
+          start: document.positionAt(m.index + m[1].length),
+          end: document.positionAt(m.index + m[1].length + m[2].length)
+        },
+        message: `"${m[2]}" is a built-in command whose behavior should not be overridden.`,
+        source: 'Paisley',
+      })
+    }
+    else if (!commandTypes.has(m[2]))
+    {
+      problems++
+
+      diagnostics.push({
+        severity: DiagnosticSeverity.Hint,
+        range: {
+          start: document.positionAt(m.index + m[1].length),
+          end: document.positionAt(m.index + m[1].length + m[2].length)
+        },
+        message: `Consider specifying the return type in a #!COMMANDS comment.`,
+        source: 'Paisley',
+      })
+
+      commandTypes.set(m[2], 'any')
+    }
+
+    if (m[3])
+    {
+      const text = m[3].substring(0, m[3].length - 3).replace(/#[^\n]+|^[ \t]+#+/g, '')
+      const beg = document.positionAt(m.index + m[1].length + m[2].length)
+      const end = document.positionAt(m.index + m[1].length + m[2].length + m[3].length - 3)
+
+      try
+      {
+        const body: Function = eval(text)
+        if (typeof body === 'function')
+        {
+          if (!commandBodies.has(m[2])) commandBodies.set(m[2], [])
+          commandBodies.get(m[2])?.push(body)
+        }
+        else
+        {
+          diagnostics.push({
+            severity: DiagnosticSeverity.Error,
+            range: {
+              start: beg,
+              end: end,
+            },
+            message: `Body must be a JavaScript function, taking an array as a parameter, and returning either a string describing the error, or null for no error.`,
+            source: 'JavaScript',
+          })
+        }
+      }
+      catch (e: any)
+      {
+        diagnostics.push({
+          severity: DiagnosticSeverity.Error,
+          range: {
+            start: beg,
+            end: end,
+          },
+          message: `${e.message}`,
+          source: 'JavaScript',
+        })
+      }
+    }
+  }
+}
 
 
 // Make the text document manager listen on the connection
