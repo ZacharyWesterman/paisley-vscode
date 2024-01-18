59cecf8779d80f686c877bd81ac0cad33d9228e5
Zachary Westerman <westerman.zachary@gmail.com>
2024-01-17 21:46:36 -0600

    Add linting for command debug info

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,6 +5,11 @@ import {
   InitializeParams,
   TextDocumentSyncKind,
   InitializeResult,
+  CompletionItem,
+  CompletionItemKind,
+  TextDocumentPositionParams,
+  Diagnostic,
+  DiagnosticSeverity,
 } from "vscode-languageserver/node";
 
 import { TextDocument } from "vscode-languageserver-textdocument";
@@ -27,10 +32,87 @@ connection.onInitialize((params: InitializeParams) => {
 });
 
 documents.onDidChangeContent((change) => {
-  connection.window.showInformationMessage(
-    "onDidChangeContent: " + change.document.uri
-  );
+  validateDebugCommands(change.document)
 });
+
+const maxProblems = 500
+let commandTypes = new Map<String, String>() //Keep track of command return types.
+
+async function validateDebugCommands(document: TextDocument) : Promise<void>
+{
+  commandTypes = new Map<String, String>()
+
+  let text = document.getText()
+  let pattern = /#!COMMANDS\b.*/g
+  let m: RegExpExecArray | null
+
+  const builtInCommands = ['print', 'sleep', 'time', 'systime', 'sysdate', 'error']
+  const returnTypes = ['null', 'boolean', 'number', 'string', 'array', 'any']
+
+  let problems = 0
+  let diagnostics: Diagnostic[] = []
+  while ((m = pattern.exec(text)) && problems < maxProblems)
+  {
+    let line = m[0].substring(10)
+    let c_pattn = /\b[^:,#; \t]+\b|[:;#]/g
+    let cmd: RegExpExecArray | null
+
+    let check_return_type = false
+    let last_cmd: String | null = null
+
+    while ((cmd = c_pattn.exec(line)) && problems < maxProblems)
+    {
+      if (cmd[0] === ';' || cmd[0] === '#') break
+
+      if (cmd[0] === ':')
+      {
+        check_return_type = true
+        continue
+      }
+
+      if (check_return_type)
+      {
+        check_return_type = false
+        if (returnTypes.includes(cmd[0]))
+        {
+          if (last_cmd !== null) commandTypes.set(last_cmd, cmd[0])
+          continue
+        }
+        problems++
+
+        diagnostics.push({
+          severity: DiagnosticSeverity.Warning,
+          range: {
+            start: document.positionAt(m.index + 10 + cmd.index),
+            end: document.positionAt(m.index + 10 + cmd.index + cmd[0].length)
+          },
+          message: `Invalid return type. Must be one of ${returnTypes.join(', ')}.`,
+          source: 'Paisley'
+        })
+        continue
+      }
+
+      last_cmd = cmd[0]
+
+      if (!builtInCommands.includes(cmd[0])) continue
+      problems++
+
+      diagnostics.push({
+        severity: DiagnosticSeverity.Warning,
+        range: {
+          start: document.positionAt(m.index + 10 + cmd.index),
+          end: document.positionAt(m.index + 10 + cmd.index + cmd[0].length)
+        },
+        message: `"${cmd[0]}" is a built-in command whose behavior should not be overridden.`,
+        source: 'Paisley'
+      })
+    }
+  }
+
+  connection.sendDiagnostics({ uri: document.uri, diagnostics})
+}
+
+
 
 // Make the text document manager listen on the connection
 // for open, change and close text document events
